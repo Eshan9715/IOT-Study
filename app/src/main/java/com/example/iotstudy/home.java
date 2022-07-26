4f8f4cae9bf7b0a79cdcbc1b0866e1400dcc100e
Eshan Amanda <eshanesn97@gmail.com>
2022-07-26 07:05:08 +0530

    Adding home screen

---
+++ app/src/main/java/com/example/iotstudy/home.java
@@ -0,0 +1,59 @@
+package com.example.SmartStudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class home extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home);
+
+        tabLayout= findViewById(R.id.tablelayout);
+        viewPager2= findViewById(R.id.viewPager2);
+
+        FragmentManager fm = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fm,getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+    }
+
+}
